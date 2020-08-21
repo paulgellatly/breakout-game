856b9d545cb485f62295e17da57a0498db48cc05
Paul G <paulgellatly@icloud.com>
2020-08-21 14:46:31 +0100

    move paddle

--- script.js
+++ script.js
@@ -52,8 +52,6 @@ for (let i = 0; i < brickColumnCount; i++) {
 	}
 }
 
-console.log(bricks);
-
 // Draw ball on canvas
 function drawBall() {
 	ctx.beginPath();
@@ -91,15 +89,65 @@ function drawBricks() {
 	});
 }
 
+// Move paddle on canvas
+function movePaddle() {
+	paddle.x += paddle.dx;
+
+	// Wall detection
+	if (paddle.x + paddle.w > canvas.width) {
+		paddle.x = canvas.width - paddle.w;
+	}
+
+	if (paddle.x < 0) {
+		paddle.x = 0;
+	}
+}
+
 // Draw everything
 function draw() {
+	// clear canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
 	drawScore();
 	drawBricks();
 }
 
-draw();
+// Update canvas drawing and animation
+function update() {
+	movePaddle();
+	// Draw everything
+	draw();
+
+	requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+	if (e.key === 'Right' || e.key === 'ArrowRight') {
+		paddle.dx = paddle.speed;
+	} else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+		paddle.dx = -paddle.speed;
+	}
+}
+
+// Keyup event
+function keyUp(e) {
+	if (
+		e.key === 'Left' ||
+		e.key === 'ArrowLeft' ||
+		e.key === 'Right' ||
+		e.key === 'ArrowRight'
+	) {
+		paddle.dx = 0;
+	}
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => {
